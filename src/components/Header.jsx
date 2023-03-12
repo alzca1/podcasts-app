8fc6f2e6ed6dc620acd85defd0603f8a1e1d5c56
Alvaro Zajara Camilo <alvaro.zajara@atos.net>
2023-03-12 09:00:10 +0100

    configuración Header y Layout para mostrar enlace y rutas mediante Outlet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='Header'>
+        <Link to="/">Podcaster</Link>
+    </div>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 export default function Layout() {
   return (
-    <div>Layout</div>
-  )
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
