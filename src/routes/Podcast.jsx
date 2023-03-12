8a80b3ac49d1162fd560c5dac937eae673cd4d3d
Alvaro Zajara Camilo <alvaro.zajara@atos.net>
2023-03-12 21:28:25 +0100

    añadida lógica para presentación de episodios y su información básica

--- src/routes/Podcast.jsx
+++ src/routes/Podcast.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import usePodcasts from '../hooks/usePodcasts';
+import {format, intervalToDuration} from 'date-fns'
 
 export default function Podcast() {
-  const { podcastId } = useParams();
+  const { podcastId, episodeId } = useParams();
   const { fetchPodcastInfo, currentPodcast } = usePodcasts();
   const [podcastWrapper, setPodcastWrapper] = useState(null);
 
@@ -19,24 +20,56 @@ export default function Podcast() {
     console.log('currentPodcast', currentPodcast);
   }, [currentPodcast]);
 
+
+  function getEpisodeDuration(durationMiliseconds) {
+    let duration = intervalToDuration({ start: 0, end: durationMiliseconds });
+
+    if (duration) {
+      const { hours, minutes, seconds } = duration;
+
+      if (hours) {
+        return `${hours}:${minutes}:${seconds}`;
+      }
+      return `${minutes}:${seconds}`;
+    }
+  }
+
   return (
     <>
-      {currentPodcast && (
-        <div className='Podcast'>
+      <div className='Podcast'>
+        {currentPodcast && (
           <div className='podcastWrapper-container'>
             <img
               src={currentPodcast?.podcastInfo['im:image'][2].label}
               alt={`${currentPodcast?.episodesInfo.collectionName} podcast cover`}
             />
-            <hr />
             <p>{currentPodcast?.podcastInfo.title.label}</p>
             <span>by: {currentPodcast?.podcastInfo['im:artist'].label}</span>
-            <hr />
             <span>Description:</span>
             <p>{currentPodcast?.podcastInfo?.summary?.label}</p>
           </div>
-        </div>
-      )}
+        )}
+        {currentPodcast && !episodeId && (
+          <div className='episodes-container'>
+            <div className='episodesCount-container'>
+              <span>Episodes: {currentPodcast.episodesInfo.resultCount}</span>
+            </div>
+            <div className='episodesList-container'>
+            {currentPodcast?.episodesInfo?.results.map((podcast) => {
+              return (
+                <Link to={`/podcast/${podcastId}/episode/${podcast.trackId}`} key={podcast.trackId} onClick={() => handleEpisodeInfo(podcast.trackId)}>
+                  <div>
+                    <p>{podcast.trackName}</p>
+                    <span>{format(new Date(podcast.releaseDate), 'dd/M/yyyy')}</span>
+                    <span>{getEpisodeDuration(podcast.trackTimeMillis)}</span>
+                  </div>
+                </Link>
+              );
+            })}
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
