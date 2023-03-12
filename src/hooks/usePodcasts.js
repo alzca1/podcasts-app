94b38f0152277065f82e6ea70e3c623e30fa1915
Alvaro Zajara Camilo <alvaro.zajara@atos.net>
2023-03-12 09:53:42 +0100

    añadida funcionalidad en hook para comprobar datos podcast en localStorage y hacer fetch en consecuencia

--- src/hooks/usePodcasts.js
+++ src/hooks/usePodcasts.js
@@ -1,17 +1,39 @@
-import React, {useState} from 'react'; 
+import { differenceInHours } from 'date-fns';
+import React, { useState } from 'react';
 
+const podcastListURL = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
 
-export default function usePodcasts(){
+export default function usePodcasts() {
+  const [podcastsInfo, setPodcastsInfo] = useState({});
+  const [fetchIsNeeded, setFetchIsNeeded] = useState(true);
 
-    const podcastListURL = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+  function fetchShipments() {
+    const podcastsInfoFromLocalStorage = JSON.parse(localStorage.getItem('podcastsStoredInfo'));
 
-    function fetchShipments(){
-        fetch(podcastListURL)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+    if (podcastsInfoFromLocalStorage) {
+      const difference = differenceInHours(new Date(), new Date(podcastsInfoFromLocalStorage.lastRefreshed));
+
+      if (difference < 24) {
+        setPodcastsInfo(podcastsInfoFromLocalStorage);
+        return setFetchIsNeeded(false);
+      }
     }
 
-    return {fetchShipments}
+    if (fetchIsNeeded) {
+      fetch(podcastListURL)
+        .then((response) => response.json())
+        .then((data) => {
+          data.feed.lastRefreshed = new Date();
+
+          let stringifiedData = JSON.stringify(data.feed);
+
+          setPodcastsInfo(stringifiedData);
+        })
+        .catch((error) => {
+          console.log(`Ooops, there was an error: ${error}`);
+        });
+    }
+  }
+
+  return { fetchShipments, podcastsInfo };
 }
