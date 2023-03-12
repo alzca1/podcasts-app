675c07269ac334cff2edf40988ca58660f607c58
Alvaro Zajara Camilo <alvaro.zajara@atos.net>
2023-03-12 12:43:34 +0100

    Añadida estructura básica en Podcast y lógica para obtención de información de podcast

--- src/routes/Podcast.jsx
+++ src/routes/Podcast.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import usePodcasts from '../hooks/usePodcasts';
 
 export default function Podcast() {
+
+  const {podcastId} = useParams(); 
+  const {fetchPodcastInfo} = usePodcasts(); 
+
+  useEffect(() => {
+    const getPodcastInfo = async () => {
+      let podcastInfo = await fetchPodcastInfo(podcastId)
+    }
+
+    getPodcastInfo(); 
+  }, [])
   return (
     <div>Podcast</div>
   )
