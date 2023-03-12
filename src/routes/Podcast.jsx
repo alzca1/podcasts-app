38f054d8aa7fe3755b80652625ffdf9f67f623b9
Alvaro Zajara Camilo <alvaro.zajara@atos.net>
2023-03-12 13:35:06 +0100

    añadida información para el podcast wrapper de presentación en Podcast.jsx

--- src/routes/Podcast.jsx
+++ src/routes/Podcast.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import usePodcasts from '../hooks/usePodcasts';
 
 export default function Podcast() {
   const { podcastId } = useParams();
   const { fetchPodcastInfo, currentPodcast } = usePodcasts();
+  const [podcastWrapper, setPodcastWrapper] = useState(null);
 
   useEffect(() => {
     const getPodcastInfo = async () => {
@@ -17,5 +18,25 @@ export default function Podcast() {
   useEffect(() => {
     console.log('currentPodcast', currentPodcast);
   }, [currentPodcast]);
-  return <div>Podcast</div>;
+
+  return (
+    <>
+      {currentPodcast && (
+        <div className='Podcast'>
+          <div className='podcastWrapper-container'>
+            <img
+              src={currentPodcast?.podcastInfo['im:image'][2].label}
+              alt={`${currentPodcast?.episodesInfo.collectionName} podcast cover`}
+            />
+            <hr />
+            <p>{currentPodcast?.podcastInfo.title.label}</p>
+            <span>by: {currentPodcast?.podcastInfo['im:artist'].label}</span>
+            <hr />
+            <span>Description:</span>
+            <p>{currentPodcast?.podcastInfo?.summary?.label}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
