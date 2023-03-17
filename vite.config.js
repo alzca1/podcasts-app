8580a9f9ef614b751c530f9194fe14063541b652
Alvaro Zajara Camilo <alvaro.zajara@atos.net>
2023-03-17 11:43:30 +0100

    Merge commit '72555fd46c031ab4e3916add84808e8e91aac327'

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ import react from '@vitejs/plugin-react'
  import fs from 'fs/promises';
 
 export default defineConfig(() => ({
+  base: '/podcasts-app',
   test: {
     globals: true, 
      environment: 'jsdom',
